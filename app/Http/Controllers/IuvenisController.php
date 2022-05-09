6f88dd62d7749fa98f738e9a137dbabcd0d7e39a
Luís Henrique <luis.henrique-ribeiro@unesp.br>
2022-05-09 20:19:37 -0300

    Criação de Rotas e IuvenisController

---
+++ app/Http/Controllers/IuvenisController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IuvenisController extends Controller
+{
+    //index/home
+    public function index()
+    {
+        
+    }
+    //explorar
+    public function explorar(){
+
+    }
+    //texto/{filtro}
+    public function texto($filtro){
+
+    }
+    //video
+    public function video(){
+
+    }
+    //evento
+    public function evento(){
+
+    }
+    //mais/{opções}
+    public function mais($opcoes){
+
+    }
+    //pesquisar/busca
+    public function pesquisa($busca){
+
+    }
+    //login
+    public function login(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+    Criar as rotas, controllers e arquivos .balde.php
+    analisar cada caso das rotas.
+
+    //criar pasta para usuário simples e admin distintas, cada um tendo seus controladores.
+
+
+*/
+Route::get('/',['as'=>'iuvenis.index', 'uses'=>'IuvenisController@index']);
+Route::get('/explorar',['as'=>'iuvenis.explorar', 'uses'=>'IuvenisController@explorar']);
+Route::get('/texto/{filtro}',['as'=>'iuvenis.texto', 'uses'=>'IuvenisController@texto']);
+Route::get('/video',['as'=>'iuvenis.video', 'uses'=>'IuvenisController@video']);
+Route::get('/evento',['as'=>'iuvenis.evento', 'uses'=>'IuvenisController@evento']);
+Route::get('/mais/{opcoes}',['as'=>'iuvenis.mais', 'uses'=>'IuvenisController@mais']);
+Route::get('/pesquisar/{busca}',['as'=>'iuvenis.pesquisar', 'uses'=>'IuvenisController@pesquisar']);
+Route::get('/login',['as'=>'iuvenis.login', 'uses'=>'IuvenisController@login']);
+
+
+
+
+
+
+
+
