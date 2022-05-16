883588effb55c92cfa3215017eb47c825f8faed8
João Vitor M. Duque <j.guerreiro@unesp.br>
2022-05-16 16:46:07 -0300

    criação das outras migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('descricao');
             $table->date('nascimento');
             $table->string('email')->unique();
-            $table->password_hash('senha');
+            $table->string('senha');
             $table->rememberToken();
             $table->timestamps();
         });
