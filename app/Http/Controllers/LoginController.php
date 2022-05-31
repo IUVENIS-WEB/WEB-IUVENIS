ce924a53039bd53a5958ed79135621e8fc8a1054
João Vitor M. Duque <j.guerreiro@unesp.br>
2022-05-31 09:40:06 -0300

    Configuração do .ENV do email e configuração do envio de email
    Ainda há o problema do envio não ser do tipo html

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\MailController;
+use Illuminate\Support\Facades\Mail;
 
 class LoginController extends Controller
 {
@@ -18,7 +20,10 @@ class LoginController extends Controller
         $email = $req->input("email");
         //Procurando no banco o usuário a partir do email dado
         $user = null;
+
+
         //Enviar email aqui CASO O USUÁRIO EXISTA, senão apenas retornar para a view
+        Mail::to($email)->send(new MailController(["nome" => "NOME DO USUÁRIO"]));
         return view('login.confirmacaoEnvio');
     }
 }
