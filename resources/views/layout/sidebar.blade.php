0b96c4dbd0a4775d0db8f5bce1609d9c3e38f56d
Rubaao <rm.dias@unesp.br>
2022-07-11 16:57:18 -0300

    Create sidebar.blade.php

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+                <title>Document</title>
+        </head>
+            <body>
+                slv slv duquezao
+                testando 1,2,3
+            </body>
+</html>
