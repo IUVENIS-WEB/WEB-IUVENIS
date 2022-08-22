781a0d8f8cf6fbfd5a0b980382e28bdff046fbea
João Vitor M. Duque <68524267+Duqueum@users.noreply.github.com>
2022-08-22 16:33:30 -0300

    Posts agrupados por TAG

--- app/Contracts/IPostRepository.php
+++ app/Contracts/IPostRepository.php
@@ -4,4 +4,5 @@ interface IPostRepository{
     function getPosts($tagIdArray = [], $take = 10);
     function getMostViewedEscritor($take = 10);
     function getLastSavedPosts($userId, $take = 10);
+    function groupedPostsByTag($tipo = null, $take = 10);
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Contracts\IPostRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Collection;
 use App\Post;
 use Exception;
+use Illuminate\Support\Facades\Response;
+
 class PostController extends Controller
 {
     public function show(Post $post){
@@ -17,11 +20,7 @@ class PostController extends Controller
         return Post::where('excluido', false)->get();
     }
 
-    public function showAllGrouped(){
-        return collect(
-            DB::table('posts')
-            ->where('excluido', false)
-            ->get()
-        )->groupBy('tipo');
+    public function showAllGrouped(IPostRepository $postRepository){
+        return response()->json($postRepository->groupedPostsByTag());
     }
 }
