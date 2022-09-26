73e3069041dae4e2e172c21df4518c2b06b6f5f7
João Vitor M. Duque <joaovmduque@gmail.com>
2022-09-25 21:24:27 -0300

    Formatação do lint

--- resources/views/iuvenis/contato.blade.php
+++ resources/views/iuvenis/contato.blade.php
@@ -8,28 +8,28 @@
     <main class="container">
         <div class="conteudo">
             <div class="conteudo-texto">
-               <div class="texto">
-                <h1>Vamos construir algo juntos?</h1>
+                <div class="texto">
+                    <h1>Vamos construir algo juntos?</h1>
                     <p>Tem perguntas, comentários ou sugestões?
-                     Será um prazer atendê-lo! </p>
+                        Será um prazer atendê-lo! </p>
                 </div>
-                <img src="{{ URL::asset('images/assets/contato.png') }}"alt="contato" >
+                <img src="{{ URL::asset('images/assets/contato.png') }}"alt="contato">
             </div>
-            <div class="forms"> 
-            	<c>Nos deixe uma mensagem</c>
+            <div class="forms">
+                <c>Nos deixe uma mensagem</c>
                 <d>Adoraríamos ouvir de você!</d>
 
 
-                <form action="{{action('EnvioController@envioContato')}}" method="POST">
-                {{ csrf_field() }}
-                    <input type = "text" name="nome" placeholder =  " Nome " /> 
-			        <input type = "email" name="email" placeholder =  " E-mail " required/>
-			        <input type = "tel" name="tel" placeholder =  " Telefone celular " required />
-			        <textarea placeholder=" Seu comentário" name="comentario" maxlength="2000"></textarea>
-                    
-				    <input type="submit">
+                <form action="{{ action('EnvioController@envioContato') }}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="text" name="nome" placeholder=" Nome " />
+                    <input type="email" name="email" placeholder=" E-mail " required />
+                    <input type="tel" name="tel" placeholder=" Telefone celular " required />
+                    <textarea placeholder=" Seu comentário" name="comentario" maxlength="2000"></textarea>
+
+                    <input type="submit" value="Enviar">
                 </form>
-			</div>
+            </div>
         </div>
 
     </main>
