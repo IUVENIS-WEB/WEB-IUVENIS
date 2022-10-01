19c9c41656f6878165cf03f1ad9ccb5b2bb33ce1
Luís Henrique <luis.henrique-ribeiro@unesp.br>
2022-10-01 07:53:05 -0300

    criação da tabela api_tokens

---
+++ database/migrations/2022_10_01_103812_api_tokens.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ApiTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_tokens', function (Blueprint $table) {
+            $table->timestamps();
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('data_expiracao');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
