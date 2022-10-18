bc310b83064a613f53f4ec86eb5ac456de9c780a
Rubens <94070905+Rubaao@users.noreply.github.com>
2022-10-18 07:12:25 -0300

    Criação da migrate api_token

--- database/migrations/2022_10_01_103812_api_tokens.php
+++ database/migrations/2022_10_01_103812_api_tokens.php
@@ -18,6 +18,7 @@ class ApiTokens extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('token')->index();
             $table->date('data_expiracao');
         });
     }
