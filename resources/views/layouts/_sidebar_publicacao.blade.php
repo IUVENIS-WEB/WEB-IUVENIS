0c949379cbbe1c526242ff571cb22d9cc4854a0a
João Vitor M. Duque <joaovmduque@gmail.com>
2022-10-26 18:44:51 -0300

    Mudança na sidebar  de publicação para facilitar a criação de novos itens

--- resources/views/layouts/_sidebar_publicacao.blade.php
+++ resources/views/layouts/_sidebar_publicacao.blade.php
@@ -1,62 +1,63 @@
+@php
+    //Todos os itens da sidebar estão dispostos aqui para melhor organização
+    $items = [
+        [
+            'tipo' => 'artigo',
+            'route' => route('iuvenis.publicacoes_texto'),
+            'icon' => asset('assets/artigo_icon.svg'),
+            'text' => 'Textos',
+        ],
+        [
+            'tipo' => 'Video',
+            'route' => route('iuvenis.publicacoes_video'),
+            'icon' => asset('assets/video_icon.svg'),
+            'text' => 'Vídeos',
+        ],
+        [
+            'tipo' => 'Evento',
+            'route' => route('iuvenis.publicacoes_evento'),
+            'icon' => asset('assets/evento_icon.svg'),
+            'text' => 'Eventos',
+        ],
+        [
+            'tipo' => 'tags',
+            'route' => route('adm.tags'),
+            'icon' => asset('assets/tag.svg'),
+            'text' => 'Tags',
+            'condition' => ['adm_power', true],
+        ],
+        [
+            'tipo' => 'conta',
+            'route' => route('iuvenis.publicacoes_evento'),
+            'icon' => asset('assets/config_icon.svg'),
+            'text' => 'Conta',
+        ],
+    ];
+    $matchTipo = false;
+@endphp
 <div class="sidebar">
     <div class="conteudo-sidebar">
         <div class="perfil">
-            <div class="imagem-perfil"><img src=" {{asset('images/users/' . Auth::user()->foto)}}" alt="foto de perfil"></div>
-            <h3>{{Auth::user()->nome}}</h3>
+            <div class="imagem-perfil"><img src=" {{ asset('images/users/' . Auth::user()->foto) }}" alt="foto de perfil">
+            </div>
+            <h3>{{ Auth::user()->nome }}</h3>
         </div>
         <div class="botoes">
-            @if( $tipo && $tipo == 'artigo')
-            <a href="{{ route('iuvenis.publicacoes_texto')}}">
-                <div class="botao btn-pagina-atual">
-                    <img src="{{asset('assets/artigo_icon.svg')}}" alt="">
-                    <h4>Textos</h4>
-                </div>
-            </a>
-            @else
-            <a href="{{ route('iuvenis.publicacoes_texto')}}">
-                <div class="botao">
-                    <img src="{{asset('assets/artigo_icon.svg')}}" alt="">
-                    <h4>Textos</h4>
-                </div>
-            </a>
-            @endif
-            @if($tipo && $tipo == 'Video')
-            <a href="{{ route('iuvenis.publicacoes_video')}}">
-                <div class="botao btn-pagina-atual">
-                    <img src="{{asset('assets/video_icon.svg')}}" alt="">  
-                    <h4>Vídeos</h4>
-                </div>
-            </a>
-            @else
-            <a href="{{ route('iuvenis.publicacoes_video')}}">
-                <div class="botao">
-                    <img src="{{asset('assets/video_icon.svg')}}" alt="">
-                    <h4>Vídeos</h4>
-                </div>
-            </a>
-            @endif
-
-            @if($tipo && $tipo == 'Evento')
-            <a href="{{ route('iuvenis.publicacoes_evento')}}">
-                <div class="botao btn-pagina-atual">
-                    <img src="{{asset('assets/evento_icon.svg')}}" alt="">
-                    <h4>Eventos</h4>
-                </div>
-            </a>
-            @else
-            <a href="{{ route('iuvenis.publicacoes_evento')}}">
-                <div class="botao">
-                    <img src="{{asset('assets/evento_icon.svg')}}" alt="">
-                    <h4>Eventos</h4>
-                </div>
-            </a>
-            @endif
-            <a href="">
-                <div class="botao">
-                    <img src="{{asset('assets/config_icon.svg')}}" alt="">
-                    <h4>Conta</h4>
-                </div>
-            </a>  
+            @foreach ($items as $item)
+                @php
+                    $matchTipo = $item['tipo'] == $tipo;
+                    $hasCondition = isset($item['condition']);
+                    $condition = $hasCondition ? Auth::user()[$item['condition'][0]] == $item['condition'][1] : true;
+                @endphp
+                @if ($condition)
+                    <a href="{{ $item['route'] }}">
+                        <div class="botao {{ $matchTipo ? 'btn-pagina-atual' : '' }}">
+                            <img src="{{ $item['icon'] }}" width="25" alt="">
+                            <h4>{{ $item['text'] }}</h4>
+                        </div>
+                    </a>
+                @endif
+            @endforeach
         </div>
     </div>
 </div>
