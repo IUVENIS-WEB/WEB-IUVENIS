0b524a224601460cc00b632cf281a8e30d886450
João Vitor M. Duque <joaovmduque@gmail.com>
2022-10-27 15:25:32 -0300

    Adicionando condições para os itens da sidebar

--- resources/views/layouts/_sidebar_publicacao.blade.php
+++ resources/views/layouts/_sidebar_publicacao.blade.php
@@ -1,4 +1,5 @@
 @php
+    $user = Auth::user();
     //Todos os itens da sidebar estão dispostos aqui para melhor organização
     $items = [
         [
@@ -6,31 +7,35 @@
             'route' => route('iuvenis.publicacoes_texto'),
             'icon' => asset('assets/artigo_icon.svg'),
             'text' => 'Textos',
+            'condition' => ['organizacao_id', '!=', null],
         ],
         [
             'tipo' => 'Video',
             'route' => route('iuvenis.publicacoes_video'),
             'icon' => asset('assets/video_icon.svg'),
             'text' => 'Vídeos',
+            'condition' => ['organizacao_id', '!=', null],
         ],
         [
             'tipo' => 'Evento',
             'route' => route('iuvenis.publicacoes_evento'),
             'icon' => asset('assets/evento_icon.svg'),
             'text' => 'Eventos',
+            'condition' => ['organizacao_id', '!=', null],
         ],
         [
             'tipo' => 'tags',
             'route' => route('adm.tags'),
             'icon' => asset('assets/tag.svg'),
             'text' => 'Tags',
-            'condition' => ['adm_power', true],
+            'condition' => ['adm_power', '=', true],
         ],
         [
             'tipo' => 'conta',
             'route' => route('iuvenis.publicacoes_evento'),
             'icon' => asset('assets/config_icon.svg'),
             'text' => 'Conta',
+            'condition' => ['organizacao_id', '!=', null],
         ],
     ];
     $matchTipo = false;
@@ -38,7 +43,7 @@
 <div class="sidebar">
     <div class="conteudo-sidebar">
         <div class="perfil">
-            <div class="imagem-perfil"><img src=" {{ asset('images/users/' . Auth::user()->foto) }}" alt="foto de perfil">
+            <div class="imagem-perfil"><img src=" {{ Storage::url(Auth::user()->foto) }}" alt="foto de perfil">
             </div>
             <h3>{{ Auth::user()->nome }}</h3>
         </div>
@@ -47,7 +52,18 @@
                 @php
                     $matchTipo = $item['tipo'] == $tipo;
                     $hasCondition = isset($item['condition']);
-                    $condition = $hasCondition ? Auth::user()[$item['condition'][0]] == $item['condition'][1] : true;
+                    $condition = true;
+                    if ($hasCondition) {
+                        $conditionRules = $item['condition'];
+                        switch ($conditionRules[1]) {
+                            case '=':
+                                $condition = $user[$conditionRules[0]] == $conditionRules[2];
+                                break;
+                            case '!=':
+                                $condition = $user[$conditionRules[0]] != $conditionRules[2];
+                                break;
+                        }
+                    }
                 @endphp
                 @if ($condition)
                     <a href="{{ $item['route'] }}">
